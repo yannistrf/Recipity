6b703c28f262a54398d3fa014072a6518255274e
yannistrf <yannistrf@gmail.com>
2025-03-15 13:39:32 +0200

    Add User model and enable message flashing

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -16,4 +18,20 @@ def logout():
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if password1 != password2:
+            flash("Passwords dont match", category="error")
+            return redirect(url_for("auth.sign_up"))
+        
+        new_user = User(username=username, password=password1)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Account created", category="success")
+        return redirect(url_for("routes.home"))
+
     return render_template("sign_up.html")
--- app/models.py
+++ app/models.py
@@ -4,3 +4,8 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     desc = db.Column(db.String(1000), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
