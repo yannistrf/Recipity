4d24bdd15f991451951cca11d6e642a7c0ad5f22
yannistrf <yannistrf@gmail.com>
2025-03-16 19:36:44 +0200

    Made recipe view page

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from . import db
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
-    desc = db.Column(db.String(1000), nullable=False)
+    desc = db.Column(db.Text, nullable=False)
     photo_path = db.Column(db.String(1000), nullable=True)
     user_id = db.Column(db.String(128), db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', back_populates='recipes')
